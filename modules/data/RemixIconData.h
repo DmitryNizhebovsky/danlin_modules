394ecc6b2d6f0cf3990ba03889e036d51986c0f8
Dmitry Nizhebovsky <dmitry.nizhebovsky@gmail.com>
2020-04-18 00:08:55 +0300

    Generated files for the new font

---
+++ modules/data/RemixIconData.h
@@ -0,0 +1,8 @@
+// IMPORTANT! This file is auto-generated see extras/RemixIconMaker
+
+#pragma once
+
+namespace RemixIconData {
+	extern const char*	remixiconwebfont_ttf;
+	const int			remixiconwebfont_ttfSize = 363628;
+}
